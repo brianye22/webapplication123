7e5d99faa760f2231f9699e1fcc2b4fcb944216c
iulian.cozma <iulian.cozma@gmail.com>
2017-12-11 16:51:13 +0200

    app settings; send controller & view & model

---
+++ WebApplication1/Controllers/BotController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BotController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Send(string msg = "")
+        {
+            string botId = Properties.Resources.bot_id;
+  
+            var x = new System.Collections.Specialized.NameValueCollection();
+            x.Add("api_key", Properties.Resources.bot_api_key);
+            x.Add("user_id", "Anonymous");
+            x.Add("message", msg);
+
+            using (var client = new System.Net.WebClient())
+            {
+                client.UploadValues(Properties.Resources.bot_url, "POST",  x);
+            }
+
+            return Json(x.ToString());
+        }
+
+        public IActionResult SendMessage(WebApplication1.Models.BotMessageModel model)
+        {
+            if (ModelState.IsValid)
+            {   //TODO: 
+                Send(model.message);
+            }
+
+            return View("SendMessage", model);
+
+        }
+        public IActionResult SM()
+        {
+            return View();
+        }
+
+    }
+}
